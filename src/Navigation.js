3e86f3b0bc8da7e0299c39e950033cd2710159ae
hkim532 <68993476+hajineats@users.noreply.github.com>
2022-02-26 01:14:01 +1300

    Add favicon to navbar

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,12 +1,13 @@
-import {Link} from "react-router-dom";
 
+import Favicon from './favicon.png'
 export function Navigation() {
     return (
         <nav className="navbar bg-dark navbar-dark">
             <div className="container">
-                <Link to="/">
-                    <span className="navbar-brand">The NZPMC</span>
-                </Link>
+                <a href={"https://www.nzpmc.com"} className="navbar-brand" style={{display:"flex", alignItems:"center"}}>
+                    <img src={Favicon} width={40} height={40} style={{marginRight:"10px"}}/>
+                    The NZPMC
+                </a>
             </div>
         </nav>
     );
